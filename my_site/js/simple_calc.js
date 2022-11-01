7d983d6ca8b34f6a8bf31ff92a8a1d796b97c375
gochuicod <gochuicodarelleq@gmail.com>
2022-11-01 17:48:10 +0800

    DG Patch 3.2.3

--- my_site/js/simple_calc.js
+++ my_site/js/simple_calc.js
@@ -1,28 +1,30 @@
-let txtArea = [], temp = [], output = document.querySelector("#txtArea");
-output.disabled = true;
+const textArea = {
+    equation: [],
+    output: document.querySelector(".txtArea"),
+    append: character => { textArea.equation.push(character); textArea.updateTextArea(); },
+    equals: () => textArea.output.value = `${textArea.evaluate(textArea.equation.join(""))}`,
+    updateTextArea: () => textArea.output.value = `${textArea.equation.join("")}`,
+    evaluate: fn => { return new Function('return ' + fn)(); },
+    clearArea: () => { textArea.equation.splice(0,textArea.equation.length); textArea.updateTextArea(); }
+}
 
-let equals = () => output.value = `${evaluate(txtArea.join(""))}`;
-let updateTextArea = () => output.value = `${txtArea.join("")}`;
-let evaluate = fn => { return new Function('return ' + fn)(); }
-let clearArea = () => { txtArea.splice(0,txtArea.length); updateTextArea(); }
-
-document.addEventListener("keydown", (e) => {
-    if(e.key === "Backspace") { txtArea.pop(); updateTextArea() }
-    if(e.key === "Enter") equals()
-    if(e.key === "c") clearArea()
-    if(e.key === "/") { txtArea.push('/'); updateTextArea() }
-    if(e.key === "+") { txtArea.push('+'); updateTextArea() }
-    if(e.key === "-") { txtArea.push('-'); updateTextArea() }
-    if(e.key === "*" || e.key === "x") { txtArea.push('*'); updateTextArea() }
-    if(e.key === "^") { txtArea.push('**'); updateTextArea() }
-    if(e.key === "0") { txtArea.push('0'); updateTextArea() }
-    if(e.key === "1") { txtArea.push('1'); updateTextArea()}
-    if(e.key === "2") { txtArea.push('2'); updateTextArea()}
-    if(e.key === "3") { txtArea.push('3'); updateTextArea()}
-    if(e.key === "4") { txtArea.push('4'); updateTextArea()}
-    if(e.key === "5") { txtArea.push('5'); updateTextArea()}
-    if(e.key === "6") { txtArea.push('6'); updateTextArea()}
-    if(e.key === "7") { txtArea.push('7'); updateTextArea()}
-    if(e.key === "8") { txtArea.push('8'); updateTextArea()}
-    if(e.key === "9") { txtArea.push('9'); updateTextArea()}
+document.addEventListener("keydown", e => {
+    if(e.key === "Backspace") { textArea.equation.pop(); textArea.updateTextArea(); }
+    if(e.key == "Enter") textArea.equals()
+    if(e.key === "c") textArea.clearArea()
+    if(e.key === "/") textArea.append('/')
+    if(e.key === "+") textArea.append('+')
+    if(e.key === "-") textArea.append('-')
+    if(e.key === "*" || e.key === "x") textArea.append('*')
+    if(e.key === "^") textArea.append('**')
+    if(e.key === "0") textArea.append('0')
+    if(e.key === "1") textArea.append('1')
+    if(e.key === "2") textArea.append('2')
+    if(e.key === "3") textArea.append('3')
+    if(e.key === "4") textArea.append('4')
+    if(e.key === "5") textArea.append('5')
+    if(e.key === "6") textArea.append('6')
+    if(e.key === "7") textArea.append('7')
+    if(e.key === "8") textArea.append('8')
+    if(e.key === "9") textArea.append('9')
 });
